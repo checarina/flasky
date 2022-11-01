63d26c93ae24d16860eb96952ceecb9f1a25a1ba
Carina <checarina@gmail.com>
2022-11-01 11:43:50 -0700

    setting up env variables

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,29 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
+load_dotenv()
 
 
-def create_app():
+def create_app(testing = None):
     # __name__ stores the name of the module we're in
     app = Flask(__name__)
 
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:postgres@localhost:5432/bikes_development"
-    
+
+    if testing is None:
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    else:
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
+
     from app.models.bike import Bike
 
     db.init_app(app)
     migrate.init_app(app, db)
-
-    
 
     from .routes.bike import bike_bp
     app.register_blueprint(bike_bp)
