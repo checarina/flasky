86764515c3d043858bab29d8dba392868b27a197
Carina <checarina@gmail.com>
2022-11-01 10:56:11 -0700

    belatedly committing changes from livecode

--- app/routes/bike.py
+++ app/routes/bike.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, abort, make_response
 from app import db
 from app.models.bike import Bike
 
@@ -18,6 +18,21 @@ from app.models.bike import Bike
 
 bike_bp = Blueprint("bike_bp", __name__, url_prefix="/bike")
 
+def get_one_bike_or_abort(bike_id):
+    try:
+        bike_id = int(bike_id)
+    except ValueError:
+        response_str = f"Invalid bike_id: `{bike_id}`. ID must be an integer."
+        abort(make_response(jsonify({"message": response_str}), 400))
+    
+    matching_bike = Bike.query.get(bike_id)
+
+    if not matching_bike:
+        response_str = f"Bike with id `{bike_id}` was not found in the database."
+        abort(make_response(jsonify({"message": response_str}), 404))
+    
+    return matching_bike
+
 @bike_bp.route("", methods = ["POST"])
 def add_bike():
     request_body = request.get_json()
@@ -35,7 +50,12 @@ def add_bike():
 
 @bike_bp.route("", methods=["GET"])
 def get_all_bikes():
-    bikes = Bike.query.all()
+    name_param = request.args.get("name")
+    bikes = Bike.query.filter_by(name = name_param)
+
+
+    if name_param is None:
+        bikes = Bike.query.all()
 
     response = []
     for bike in bikes:
@@ -49,32 +69,36 @@ def get_all_bikes():
         response.append(bike_dict)
     return jsonify(response), 200
 
+@bike_bp.route("/<bike_id>", methods=["GET"])
+def get_one_bike(bike_id):
+    chosen_bike = get_one_bike_or_abort(bike_id)
+    bike_dict = {
+            "id": chosen_bike.id,
+            "name": chosen_bike.name,
+            "price": chosen_bike.price,
+            "size": chosen_bike.size,
+            "type": chosen_bike.type
+        }
+    return jsonify(bike_dict), 200
 
-# @bike_bp.route("/<bike_id>", methods=["GET"])
-# def get_one_bike(bike_id):
-#     #see if bike_id can be converted to an integer
-#     #try-except: try to convert to an int, if error occurs, catch it and raise 400 error with message
-#     try:
-#         bike_id = int(bike_id)
-#     except ValueError:
-#         response_str = f"Invalid bike_id: `{bike_id}`. ID must be an integer"
-#         return jsonify({"message": response_str}), 400
-#     #after the try-except: bike_id will be a valid int
+@bike_bp.route("/<bike_id>", methods=["PUT"])
+def update_bike_with_new_vals(bike_id):
+    select_bike = get_one_bike_or_abort(bike_id)
+    request_body = request.get_json()
+    if "name" not in request_body or "size" not in request_body or "price" not in request_body or "type" not in request_body:
+        return jsonify({"message": "All bike parameters must be specified."}, 400)
+    select_bike.name = request_body["name"]
+    select_bike.size = request_body["size"]
+    select_bike.price = request_body["price"]
+    select_bike.type = request_body["type"]
 
-#     #looping through data to find a bike with matching bike_id
-#     #if found: return that bike's data with 200 response code
-#     for bike in bikes:
-#         if bike.id == bike_id:
-#             bike_dict = {
-#                 "id": bike.id,
-#                 "name": bike.name,
-#                 "price": bike.price,
-#                 "size": bike.size,
-#                 "type": bike.type
-#                 }
-#             #return in the if block
-#             return jsonify(bike_dict), 200
-        
-#     #after the loop: the bike with matching bike_id was not found, we will raise 404 error with message
-#     response_message = f"Could not find bike with ID {bike_id}"
-#     return jsonify({"message":response_message}), 404
+    db.session.commit()
+
+    return jsonify({"message": f"Sucessfully replaced bike with id {bike_id}."}, 200)
+
+@bike_bp.route("/<bike_id>", methods=["DELETE"])
+def delete_one_bike(bike_id):
+    select_bike = get_one_bike_or_abort(bike_id)
+    db.session.delete(select_bike)
+    db.session.commit()
+    return jsonify({"message": f"Successfully deleted bike with id `{bike_id}`."}, 200)
