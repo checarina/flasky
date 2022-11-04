877f0d5fee487946624ccbbff008f800d505282f
Jerica Huang <jerica.huang@gmail.com>
2022-11-04 11:05:52 -0700

    Refactored!

--- app/models/bike.py
+++ app/models/bike.py
@@ -7,3 +7,26 @@ class Bike(db.Model):
     size = db.Column(db.Integer)
     type = db.Column(db.String)
 
+    def to_dict(self):
+        bike_dict = {
+            "id": self.id,
+            "name": self.name,
+            "price": self.price,
+            "size": self.size,
+            "type": self.type
+        }
+        return bike_dict
+
+    @classmethod
+    def from_dict(cls, data_dict):
+        #check data_dict has all valid bike attributes
+        #helps prevent KeyError
+        if "name" in data_dict and "price" in data_dict and "size" in data_dict and "type" in data_dict:
+            new_obj = cls(name=data_dict["name"], 
+            price=data_dict["price"], 
+            size=data_dict["size"], 
+            type=data_dict["type"])
+            
+            return new_obj
+        #if not, can look into raising an error with make_response and abort(); did not have time in class.
+
